222d3e839a120cd01deab5db35c7ab4a4fa3d589
sake92 <sake.etf@gmail.com>
2016-03-20 18:19:58 +0100

    Added time and checkboxes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,16 +11,19 @@ import forms.StudentForm
 
 class Application @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
+  val allTeachers = List("Sake", "Amer", "Meho")
+  
   def showCreate = Action {
-    Ok(views.html.create(StudentForm.form))
+    Ok(views.html.create(StudentForm.form, allTeachers))
   }
 
   def create = Action { implicit request =>
     StudentForm.form.bindFromRequest.fold(
       formWithErrors => {
         println("ERROR")
+        println(formWithErrors.data)
         // binding failure, you retrieve the form containing errors:
-        BadRequest(views.html.create(formWithErrors))
+        BadRequest(views.html.create(formWithErrors, allTeachers))
       },
       studentData => {
         /* binding success, you get the actual value. */
--- app/forms/StudentForm.scala
+++ app/forms/StudentForm.scala
@@ -3,12 +3,15 @@ package forms
 import play.api.data._
 import play.api.data.Forms._
 import java.util.Date
+import org.joda.time.DateTime
 
 case class StudentData(
   id: Int,
   name: String,
   dateOfBirth: Date,
+  averageTime: DateTime,
   address: AddressData,
+  favouriteTeachers: List[String],
   grades: List[GradeData])
 
 case class AddressData(
@@ -26,10 +29,12 @@ object StudentForm {
       "id" -> number(min = 1),
       "name" -> text,
       "dateOfBirth" -> date,
+      "averageTime" -> jodaDate("hh:mm"),
       "address" -> mapping(
         "street" -> text,
         "number" -> number(min = 1)
       )(AddressData.apply)(AddressData.unapply),
+      "favouriteTeachers" -> list(text),
       "grades" -> list(mapping(
         "subject" -> text,
         "grade" -> number
